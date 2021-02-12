aa452f2d98a1a1adbba667eb884f12db07228e80
Andy Lawson <alawson89@gmail.com>
2021-02-12 09:29:55 -0800

    add function getElements function/successful api call

--- src/main.js
+++ src/main.js
@@ -4,8 +4,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchanger from './currency-exchanger.js';
 
+function getElements(response) {
+  if (response) {
+    $('.showExchange').text(`Exchange Rate for ${response.base_code} is ${response.conversion_rates.KRW}`);
+    console.log(response.conversion_rates.USD);
+  } else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
+}
 
-
-$(document).ready(function () {
-
+$(document).ready(function() {
+  $("#exchangeRate").click(function() {
+    let dollars = parseInt($('#amount').val());
+    CurrencyExchanger.getExchange()
+      .then(function(response) {
+        getElements(response);
+      });
+  });
 });
