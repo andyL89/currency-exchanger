8f737499c427191f01ab67c0ac9505b8733b6b53
Andy Lawson <alawson89@gmail.com>
2021-02-12 15:38:23 -0800

    add mvp of currency exchanger w/ error messages

--- src/main.js
+++ src/main.js
@@ -4,28 +4,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchanger from './currency-exchanger.js';
 
+function clearFields() {
+  $('.showErrors').text("");
+  $('.showExchange').text("");
+}
+
 function getElements(response) {
   if (response.result === "success") {
-    $('.showExchange').text(` ${response.base_code} to ${response.target_code} = ${response.conversion_result}`);
-  } else {
-    $('.showErrors').text(`There was an error: ${response}`);
+    clearFields();
+    $('.showExchange').text(`${Math.round(response.conversion_result / response.conversion_rate)} ${response.base_code} to ${response.target_code} = ${response.conversion_result}`);
+  } else if (response.result === "error") {
+    clearFields();
+    $('.showErrors').text(`There was an error: The North Korean Won is not a recognized currency in the world economy.`);
     console.log(response);
   }
 }
 
-
-// selection.addEventListener('change', () => {
-//   result.innerText = selection.options[selection.selectedIndex].value;
-// });
 let selection = document.querySelector('select');
 let result = document.querySelector('h3');
 
-$(selection).change(function () {
+$(selection).change(function (event) {
+  event.preventDefault();
   result.innerText = selection.options[selection.selectedIndex].value;
   let amount = $('#dollarAmount').val();
   let currencyCode = result.innerText;
   CurrencyExchanger.getExchange(currencyCode, amount)
     .then(function (response) {
+      if (amount < 1) {
+        alert("Please enter a valid dollar amount.");
+      }
       getElements(response);
     });
 });
