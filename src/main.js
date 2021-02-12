319131f0a4d2524d469fe5e32e5ea329976a84ab
Andy Lawson <alawson89@gmail.com>
2021-02-12 13:26:22 -0800

    aretrieve values from select options

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchanger from './currency-exchanger.js';
 
-
-
 function getElements(response) {
   if (response) {
     $('.showExchange').text(`Exchange Rate for ${response.base_code} to ${response.target_code} is ${response.conversion_rate}`);
@@ -14,12 +12,14 @@ function getElements(response) {
   }
 }
 
-$(document).ready(function() {
-  $(".dropdown-item").click(function() {
-    let currencyCode = ($('.dropdown-item').val());
-    CurrencyExchanger.getExchange(currencyCode)
-      .then(function(response) {
-        getElements(response);
-      });
-  });
+
+
+let selection = document.querySelector('select');
+let result = document.querySelector('h2');
+
+selection.addEventListener('change', () => {
+  result.innerText = selection.options[selection.selectedIndex].value;
 });
+    // CurrencyExchanger.getExchange(currencyCode)
+    //   .then(function(response) {
+    //     getElements(response);
