7336ecf19082bd78b2dfcc9120760235f6adae11
Andy Lawson <alawson89@gmail.com>
2021-02-12 16:47:13 -0800

    create working convert button

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ function getElements(response) {
     $('.showExchange').text(`${Math.round(response.conversion_result / response.conversion_rate)} ${response.base_code} = ${response.conversion_result} ${response.target_code}`);
   } else if (response.result === "error") {
     clearFields();
-    $('.showErrors').text(`Error: The North Korean Won is not a recognized currency in the world economy.`);
+    $('.showErrors').text(`ERROR: CURRENCY UNRECOGNIZED`);
     console.log(response);
   }
 }
@@ -23,16 +23,22 @@ function getElements(response) {
 let selection = document.querySelector('select');
 let result = document.querySelector('h3');
 
-$(selection).change(function (event) {
-  event.preventDefault();
-  result.innerText = selection.options[selection.selectedIndex].value;
-  let amount = $('#dollarAmount').val();
-  let currencyCode = result.innerText;
-  CurrencyExchanger.getExchange(currencyCode, amount)
-    .then(function (response) {
-      if (amount < 1) {
-        alert("Please enter a valid dollar amount.");
-      }
-      getElements(response);
-    });
+$(selection).ready(function () {
+  $("#getExchange").click(function () {
+    result.innerText = selection.options[selection.selectedIndex].value;
+    let amount = $('#dollarAmount').val();
+    let currencyCode = result.innerText;
+    CurrencyExchanger.getExchange(currencyCode, amount)
+      .then(function (response) {
+        if (amount < 1) {
+          alert("Please enter a valid dollar amount.");
+        }
+        getElements(response);
+      });
+  });
 });
+
+  // $(document).ready(function () {
+  //   $("#getExchange").click(function () {
+  //     $(".showExchange").show;
+  //   });
